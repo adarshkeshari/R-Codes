c7f45b657de347008908bd34b5fb4f738dc97eb9
adarshkeshari <74828604+adarshkeshari@users.noreply.github.com>
2020-11-21 21:27:09 +0530

    Add files via upload

---
+++ 1-R-Intro- 21 Nov.R
@@ -0,0 +1,31 @@
+# - Commenting in R- Documentation
+# - Shortcuts-- Ctrl+L--- Clear the Screen
+# - Ctrl+Enter---- Executing a particular line of code
+a=5
+a
+b=10
+b
+z=a*b
+z
+y<-a-b
+y
+class(a)
+a= "Hello"
+a= TRUE
+
+# object assignment and simple calculation
+x=2
+y=3
+x+y
+p=x+y
+x/y
+x^y
+sqrt(x)
+x^y
+exp(x)
+log(x,base =exp (1))
+log10(x)
+x**y
+x**y
+x+y
+x**y
